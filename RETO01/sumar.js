4b33066b3514d7cf2c861c0e46b301173b319ef8
Jsierra0616 <arqjorgesierra@gmail.com>
2024-06-05 09:55:05 -0500

    Delete RETO01 directory

--- RETO01/sumar.js
+++
@@ -1,6 +0,0 @@
-function calcularSuma() {
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
-    const resultado = num1 + num2;
-    document.getElementById('resultado').textContent = 'Resultado: ' + resultado;
-}
